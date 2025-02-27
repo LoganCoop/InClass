1ceae0586447c78b1bfe38beb70977f0f9197b6a
LoganCoop <lpcooper@gmail.com>
2025-02-27 14:10:39 -0500

    small change

--- server/index.js
+++ server/index.js
@@ -1,19 +1,18 @@
 // Initialize the server Logan Cooper
 
 
-// Load the http module to create an http server.
-const http = require('http');
+// Load express module to create a server
+const express = require('express');
 const PORT = 8000;
 
+const app = express();
 
-const server = http.createServer((request, response) => {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hello World!');
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
-
 // Listen on port 8000, IP defaults to
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/`);
 });
 
